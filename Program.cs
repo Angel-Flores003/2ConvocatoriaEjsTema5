d14b1b92625b63f1f87f6f6701537c51ca70dafb
AngelF <angel-abraham.flores.7e8@itb.cat>
2025-06-05 16:58:52 +0200

    Complete 5.1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace EjsTema5
+﻿using EjsTema5.DTOs;
+
+namespace EjsTema5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StudentDTO student = new StudentDTO(1, "John", "Doe");
+            Console.WriteLine($"Student ID: {student.Id}, Name: {student.FirstName} {student.LastName}");
         }
     }
 }
