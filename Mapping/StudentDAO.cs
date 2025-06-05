38d591b6a48a123b873d3b81c93eb10c7f1df7c0
AngelF <angel-abraham.flores.7e8@itb.cat>
2025-06-05 17:28:00 +0200

    Complete 5.2

---
+++ Mapping/StudentDAO.cs
@@ -0,0 +1,75 @@
+﻿using EjsTema5.DAOs;
+using EjsTema5.DTOs;
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjsTema5.Mapping
+{
+    public class StudentDAO : IStudentDAO
+    {
+        private readonly string _ConnectionString;
+        public StudentDAO(string connectionstring)
+        {
+            _ConnectionString = connectionstring;
+        }
+
+        public void AddStudent(StudentDTO students)
+        {
+            using (SqlConnection connection = new SqlConnection(_ConnectionString))
+            {                
+                string query = "INSERT INTO Students (Id, FirstName, LastName) VALUES (@Id, @FirstName, @LastName)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", students.Id);
+                    command.Parameters.AddWithValue("@FirstName", students.FirstName);
+                    command.Parameters.AddWithValue("@LastName", students.LastName);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<StudentDTO> GetAllStudents()
+        {
+            List<StudentDTO> students = new List<StudentDTO>();
+            using (SqlConnection connection = new SqlConnection(_ConnectionString))
+            {
+                connection.Open();
+                string query = "SELECT Id, FirstName, LastName FROM Students";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string firstName = reader.GetString(1);
+                            string lastName = reader.GetString(2);
+                            students.Add(new StudentDTO(id, firstName, lastName));
+                        }
+                    }
+                }
+            }
+            return students;
+        }
+
+        public void DeleteStudent(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(_ConnectionString))
+            {
+                connection.Open();
+                string query = "DELETE FROM Students WHERE Id = @Id";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using EjsTema5.DTOs;
+﻿using EjsTema5.DAOs;
+using EjsTema5.DTOs;
+using EjsTema5.Mapping;
 
 namespace EjsTema5
 {
@@ -6,8 +8,27 @@ namespace EjsTema5
     {
         static void Main(string[] args)
         {
-            StudentDTO student = new StudentDTO(1, "John", "Doe");
-            Console.WriteLine($"Student ID: {student.Id}, Name: {student.FirstName} {student.LastName}");
+            string connectionString = "Data Source=localhost;Initial Catalog=EjsTema5;Integrated Security=True";
+            IStudentDAO studentDAO = new StudentDAO(connectionString);
+
+            // Afegir estudiant
+            StudentDTO newStudent = new StudentDTO(1, "John", "Doe");
+            studentDAO.AddStudent(newStudent);
+
+            Console.WriteLine("Student added successfully.");
+
+            //Mostrar estudiants
+            List<StudentDTO> students = studentDAO.GetAllStudents();
+            Console.WriteLine("List of students:");
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Id: {student.Id}, First Name: {student.FirstName}, Last Name: {student.LastName}");
+            }
+
+            // Eliminar estudiant
+            int studentIdToDelete = 1; // Assuming we want to delete the student with Id 1
+            studentDAO.DeleteStudent(studentIdToDelete);
+            Console.WriteLine($"Student with Id {studentIdToDelete} deleted successfully.");
         }
     }
 }
