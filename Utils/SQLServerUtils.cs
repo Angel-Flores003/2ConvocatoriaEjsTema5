543934ef24af84a3240aee17721f7245470d2761
AngelF <angel-abraham.flores.7e8@itb.cat>
2025-06-05 17:38:05 +0200

    nope 5.3

---
+++ Utils/SQLServerUtils.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace daoexample.Persistence.Utils
+{
+    public class SQLServerUtils
+    {
+        public static string OppenConnection()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+            return config.GetConnectionString("DefaultConnection");
+        }
+    }
+}
